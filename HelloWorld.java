38f88228a1f1071ae7ddae7f25d9575fe7f28782
Luka <malak.raaof664@gmaul.com>
2024-12-30 13:37:28 +0200

    new print

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+	
+	void printHelloWorld(){
+	System.out.println("HELLO WORLD");
+	}
+
+
+}
+
+class HelloITI{
+
+	
+	void printHelloITI(){
+	System.out.println("HELLO GIT");
+	}
+
+
+}
+
